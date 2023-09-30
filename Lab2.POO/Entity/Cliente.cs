17930fd0c480638bbca71073e15b4353e2685d27
HelenKBC <helen452013@gmail.com>
2023-09-29 18:55:36 -0600

    Cliente

--- Lab2.POO/Entity/Cliente.cs
+++ Lab2.POO/Entity/Cliente.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Lab2.POO.Entity
 {
-    internal class Cliente
+    public class Cliente
     {
+            public int Id { get; set; }
+            public string Nombres { get; set; }
+            public string Apellidos { get; set; }
+            public string Direccion { get; set; }
+            public string Telefono { get; set; }
+            public string NIT { get; set; }
+            public string NRC { get; set; }
+        
     }
 }
